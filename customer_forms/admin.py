8e05299ab38842e919eaeeaa3ce09f8ff5e8abe1
akanshabaishwade <akanshabaishwade@gmail.com>
2024-05-22 19:58:03 +0530

    [remove] old

--- customer_forms/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CountryModel, DocumentSetModel, CustomerModel, CustomerDocumentModel
-
-admin.site.register(CountryModel)
-admin.site.register(DocumentSetModel)
-admin.site.register(CustomerModel)
-admin.site.register(CustomerDocumentModel)
