4ede21800cf22975b5fe309ad0c9d65258f340e6
Akanksha Baishwade <85228361+akanshabaishwade@users.noreply.github.com>
2024-05-24 03:15:58 +0530

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('aws/upload/', upload_file, name='upload_document'),
-    path('upload-success/', upload_success, name='upload_success'),
+    path('aws/upload/', upload_document, name='upload_document'),
     path('create_or_select_customer/', create_or_select_customer, name='create_or_select_customer'),
     path('customer_list/', customer_list, name='customer_list'),
 
-    # path('upload/', UploadDocumentView.as_view(), name='upload_document'),
-    # path('document_uploaded/', DocumentUploadedView.as_view(), name='document_uploaded'),
 ]
 
